619cf390cd9e0a43cb292fc252d69fdc131f3905
S-NCERE <33769354+S-NCERE@users.noreply.github.com>
2019-02-06 19:12:02 -0500

    addition of p5 comments

--- sketch.js
+++ sketch.js
@@ -11,22 +11,47 @@ function draw() {
     fill(218, 172, 213);
     ellipseMode(RADIUS);
     ellipse(250, 250, 150, 150);
-// top left purple circle
-    noStroke();
+// top left purple circle cut out
+    
+//    noStroke();
+//    fill(147,112,219);
+//    ellipseMode(CORNERS);
+//    ellipse(200, 200, 100, 100);
+//    
+    
+    
+ // inner circle purple 3   
+    stroke(25);
     fill(147,112,219);
-    ellipseMode(CORNERS);
-    ellipse(200, 200, 100, 100);
-
-// inner purple
+    ellipseMode(CENTER);
+    ellipse(250, 250, 220, 220);
+// inner circle pink 3    
+    stroke(25);
+    fill(218, 172, 213);
+    ellipseMode(CENTER);
+    ellipse(250, 250, 190, 190);
+// inner circle purple 2
+    stroke(25);
+    fill(147,112,219);
+    ellipseMode(CENTER);
+    ellipse(250, 250, 160, 160);
+//  inner circle pink 2
+    stroke(25);
+    fill(218, 172, 213);
+    ellipseMode(CENTER);
+    ellipse(250, 250, 130, 130);
+// inner circle purple 1
     stroke(10);
     fill(147,112,219);
 	ellipseMode(CENTER);
     ellipse(250, 250, 100, 100);
-// inner pink
+// inner circle pink 1
     stroke(25);
     fill(218, 172, 213);
     ellipseMode(CENTER);
     ellipse(250, 250, 70, 70);
+    
+    
 // crosshair dot
     fill(0);
     ellipseMode(CENTER);
@@ -45,15 +70,17 @@ function draw() {
     line(245, 170, 255, 170);
     line(245, 180, 255, 180);
     line(245, 190, 255, 190);
-
+    line(245, 210, 255, 210);
+    
     line(245, 220, 255, 220);
     line(245, 230, 255, 230);
     line(245, 240, 255, 240);
-// below dot y axis
+    
     line(245, 260, 255, 260);
     line(245, 270, 255, 270);
     line(245, 280, 255, 280);
-
+    
+    line(245, 290, 255, 290);
     line(245, 310, 255, 310);
     line(245, 320, 255, 320);
     line(245, 330, 255, 330);
@@ -73,12 +100,16 @@ function draw() {
     line(170,245, 170, 255);
     line(180,245, 180, 255);
     line(190,245, 190, 255);
+    line(210,245, 210, 255);
+    
     line(220,245, 220, 255);
     line(230,245, 230, 255);
     line(240,245, 240, 255);
     line(260,245, 260, 255);
     line(270,245, 270, 255);
     line(280,245, 280, 255);
+    
+    line(290,245, 290, 255);
     line(310,245, 310, 255);
     line(320,245, 320, 255);
     line(330,245, 330, 255);
