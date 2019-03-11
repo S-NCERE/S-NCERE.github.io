6d94030d4aad85dc57b525f4b8c053c410f72ffe
S-NCERE <33769354+S-NCERE@users.noreply.github.com>
2019-03-11 18:15:31 -0400

    first meme not done

---
+++ meme.js
@@ -0,0 +1,16 @@
+function setup() {
+    var cnv = createCanvas(800, 800);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+    cnv.parent('meme');
+    background(0);
+    img = loadImage('images/dripDex.jpg');
+    img2 = loadImage('images/dripDex.jpg');
+}
+
+function draw(){
+    //when ya drip go un noticed
+    // imgs to big must size down
+    
+}
